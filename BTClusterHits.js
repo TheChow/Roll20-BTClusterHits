bbd4ff20573a3b616f99bcbd63e978c9253f9c36
Chia-Hao Chang <chaihaoc@gmail.com>
2015-01-20 21:18:28 -0500

    Changed to revealing module pattern.  converted hits to numeric

--- BTClusterHits.js
+++ BTClusterHits.js
@@ -1,6 +1,6 @@
-var BTClusterHits = {
+var BTClusterHits = BTClusterHits || (function () {
 
-    Cluster2: function Cluster2() {
+    function cluster2() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
@@ -10,398 +10,435 @@ var BTClusterHits = {
             case 5:
             case 6:
             case 7:
-                num_hits = "1";
+                num_hits = 1;
                 break;
             case 8:
             case 9:
             case 10:
             case 11:
             case 12:
-                num_hits = "2";
+                num_hits = 2;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
+    }
 
-
-    Cluster3: function Cluster3() {
+    function cluster3() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
             case 2:
             case 3:
             case 4:
-                num_hits = "1";
+                num_hits = 1;
                 break;
             case 5:
             case 6:
             case 7:
             case 8:
             case 9:
-                num_hits = "2";
+                num_hits = 2;
                 break;
             case 10:
             case 11:
             case 12:
-                num_hits = "3";
+                num_hits = 3;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
+    }
 
-
-    Cluster4: function Cluster4() {
+    function cluster4() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
             case 2:
-                num_hits = "1";
+                num_hits = 1;
                 break;
             case 3:
             case 4:
             case 5:
             case 6:
-                num_hits = "2";
+                num_hits = 2;
                 break;
             case 7:
             case 8:
             case 9:
             case 10:
-                num_hits = "3";
+                num_hits = 3;
                 break;
             case 11:
             case 12:
-                num_hits = "4";
+                num_hits = 4;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
+    }
 
-
-    Cluster5: function Cluster5() {
+    function cluster5() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
             case 2:
-                num_hits = "1";
+                num_hits = 1;
                 break;
             case 3:
             case 4:
-                num_hits = "2";
+                num_hits = 2;
                 break;
             case 5:
             case 6:
             case 7:
             case 8:
-                num_hits = "3";
+                num_hits = 3;
                 break;
             case 9:
             case 10:
-                num_hits = "4";
+                num_hits = 4;
                 break;
             case 11:
             case 12:
-                num_hits = "5";
+                num_hits = 5;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
+    }
 
-
-    Cluster6: function Cluster6() {
+    function cluster6() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
             case 2:
             case 3:
-                num_hits = "2";
+                num_hits = 2;
                 break;
             case 4:
             case 5:
-                num_hits = "3";
+                num_hits = 3;
                 break;
             case 6:
             case 7:
             case 8:
-                num_hits = "4";
+                num_hits = 4;
                 break;
             case 9:
             case 10:
-                num_hits = "5";
+                num_hits = 5;
                 break;
             case 11:
             case 12:
-                num_hits = "6";
+                num_hits = 6;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
+    }
 
-
-    Cluster7: function Cluster7() {
+    function cluster7() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
             case 2:
             case 3:
-                num_hits = "2";
+                num_hits = 2;
                 break;
             case 4:
-                num_hits = "3";
+                num_hits = 3;
                 break;
             case 5:
             case 6:
             case 7:
             case 8:
-                num_hits = "4";
+                num_hits = 4;
                 break;
             case 9:
             case 10:
-                num_hits = "6";
+                num_hits = 6;
                 break;
             case 11:
             case 12:
-                num_hits = "7";
+                num_hits = 7;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
+    }
 
-
-    Cluster9: function Cluster9() {
+     function cluster9() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
             case 2:
             case 3:
-                num_hits = "3";
+                num_hits = 3;
                 break;
             case 4:
-                num_hits = "4";
+                num_hits = 4;
                 break;
             case 5:
             case 6:
             case 7:
             case 8:
-                num_hits = "5";
+                num_hits = 5;
                 break;
             case 9:
             case 10:
-                num_hits = "7";
+                num_hits = 7;
                 break;
             case 11:
             case 12:
-                num_hits = "9";
+                num_hits = 9;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
+    }
 
-
-    Cluster10: function Cluster10() {
+     function cluster10() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
             case 2:
             case 3:
-                num_hits = "3";
+                num_hits = 3;
                 break;
             case 4:
-                num_hits = "4";
+                num_hits = 4;
                 break;
             case 5:
             case 6:
             case 7:
             case 8:
-                num_hits = "6";
+                num_hits = 6;
                 break;
             case 9:
             case 10:
-                num_hits = "8";
+                num_hits = 8;
                 break;
             case 11:
             case 12:
-                num_hits = "10";
+                num_hits = 10;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
+    }
 
-
-    Cluster12: function Cluster12() {
+    function cluster12() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
             case 2:
             case 3:
-                num_hits = "4";
+                num_hits = 4;
                 break;
             case 4:
-                num_hits = "5";
+                num_hits = 5;
                 break;
             case 5:
             case 6:
             case 7:
             case 8:
-                num_hits = "8";
+                num_hits = 8;
                 break;
             case 9:
             case 10:
-                num_hits = "10";
+                num_hits = 10;
                 break;
             case 11:
             case 12:
-                num_hits = "12";
+                num_hits = 12;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
-
-    Cluster15: function Cluster15() {
+    }
+    
+    function cluster15() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits;
         switch (roll) {
             case 2:
             case 3:
-                num_hits = "5";
+                num_hits = 5;
                 break;
             case 4:
-                num_hits = "6";
+                num_hits = 6;
                 break;
             case 5:
             case 6:
             case 7:
             case 8:
-                num_hits = "9";
+                num_hits = 9;
                 break;
             case 9:
             case 10:
-                num_hits = "12";
+                num_hits = 12;
                 break;
             case 11:
             case 12:
-                num_hits = "15";
+                num_hits = 15;
                 break;
             default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
-    },
+    }
 
-
-    Cluster20: function Cluster20() {
+    function cluster20() {
         var roll = randomInteger(6) + randomInteger(6);
         var num_hits = 0;
         switch (roll) {
             case 2:
             case 3:
-                num_hits = "6";
+                num_hits = 6;
                 break;
             case 4:
-                num_hits = "9";
+                num_hits = 9;
                 break;
             case 5:
             case 6:
             case 7:
             case 8:
-                num_hits = "12";
+                num_hits = 12;
                 break;
             case 9:
             case 10:
-                num_hits = "16";
+                num_hits = 16;
                 break;
             case 11:
             case 12:
-                num_hits = "20";
+                num_hits = 20;
                 break;
             default:
-                num_hits = "0";
-        }
-        return num_hits;
-    },
-
-
-    Cluster30: function Cluster30() {
-        var roll = randomInteger(6) + randomInteger(6);
-        var num_hits;
-        switch (roll) {
-            case 2:
-            case 3:
-                num_hits = "10";
-                break;
-            case 4:
-                num_hits = "12";
-                break;
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-                num_hits = "18";
-                break;
-            case 9:
-            case 10:
-                num_hits = "24";
-                break;
-            case 11:
-            case 12:
-                num_hits = "30";
-                break;
-            default:
-                num_hits = "0";
-        }
-        return num_hits;
-    },
-
-
-    Cluster40: function Cluster40() {
-        var roll = randomInteger(6) + randomInteger(6);
-        var num_hits;
-        switch (roll) {
-            case 2:
-            case 3:
-                num_hits = "12";
-                break;
-            case 4:
-                num_hits = "18";
-                break;
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-                num_hits = "24";
-                break;
-            case 9:
-            case 10:
-                num_hits = "32";
-                break;
-            case 11:
-            case 12:
-                num_hits = "40";
-                break;
-            default:
-                num_hits = "0";
+                num_hits = 0;
         }
         return num_hits;
     }
-};
+
+    function cluster30() {
+        var roll = randomInteger(6) + randomInteger(6);
+        var num_hits;
+        switch (roll) {
+            case 2:
+            case 3:
+                num_hits = 10;
+                break;
+            case 4:
+                num_hits = 12;
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                num_hits = 18;
+                break;
+            case 9:
+            case 10:
+                num_hits = 24;
+                break;
+            case 11:
+            case 12:
+                num_hits = 30;
+                break;
+            default:
+                num_hits = 0;
+        }
+        return num_hits;
+    }
+
+    function cluster40() {
+        var roll = randomInteger(6) + randomInteger(6);
+        var num_hits;
+        switch (roll) {
+            case 2:
+            case 3:
+                num_hits = 12;
+                break;
+            case 4:
+                num_hits = 18;
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                num_hits = 24;
+                break;
+            case 9:
+            case 10:
+                num_hits = 32;
+                break;
+            case 11:
+            case 12:
+                num_hits = 40;
+                break;
+            default:
+                num_hits = 0;
+        }
+        return num_hits;
+    }
+    
+    function getClusterHits(shots) {
+        if (shots === null || _.isUndefined(shots)) return;
+        if (shots.indexOf("c02") === 0) {
+            return BTClusterHits.Cluster2();            
+        } else if (shots.indexOf("c03") === 0) {
+            return BTClusterHits.Cluster3();            
+        } else if (shots.indexOf("c04") === 0) {
+            return BTClusterHits.Cluster4();
+        } else if (shots.indexOf("c05") === 0) {
+            return BTClusterHits.Cluster5();
+        } else if (shots.indexOf("c06") === 0) {
+            return BTClusterHits.Cluster6();
+        } else if (shots.indexOf("c07") === 0) {
+            return BTClusterHits.Cluster7();
+        } else if (shots.indexOf("c09") === 0) {
+            return BTClusterHits.Cluster9();
+        } else if (shots.indexOf("c10") === 0) {
+            return BTClusterHits.Cluster10();
+        } else if (shots.indexOf("c12") === 0) {
+            return BTClusterHits.Cluster12();
+        } else if (shots.indexOf("c15") === 0) {
+            return BTClusterHits.Cluster15();
+        } else if (shots.indexOf("c20") === 0) {
+            return  BTClusterHits.Cluster20();
+        } else if (shots.indexOf("c30") === 0) {
+            return BTClusterHits.Cluster30();
+        } else if (shots.indexOf("c40") === 0) {
+            return  BTClusterHits.Cluster40();
+        }
+    }
+	
+	return {
+		Cluster2: cluster2,
+		Cluster3: cluster3,
+		Cluster4: cluster4,
+		Cluster5: cluster5,
+		Cluster6: cluster6,
+		Cluster7: cluster7,
+		Cluster9: cluster9,
+		Cluster10: cluster10,
+		Cluster12: cluster12,
+		Cluster15: cluster15,
+		Cluster20: cluster20,
+		Cluster30: cluster30,
+		Cluster40: cluster40,
+		GetClusterHits: getClusterHits
+	};
+})();
 
 on("chat:message", function (msg) {
     if (msg.type == "api") {
@@ -461,7 +498,7 @@ on("chat:message", function (msg) {
         }
 
         if (isCluster) {
-            var loc = msg.content.split(' ')[1] || "f";
+            var loc = msg.content.split(' ')[1].toLowerCase() || "f";
             var hitLocCallback, hitTarget;
             switch (loc) {
                 case "l":
